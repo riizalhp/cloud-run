b9cb9731f9e6a4907d2f4d40a64641cd08ca9385
Rizal Hanifa Pratama <117881668+riizalhp@users.noreply.github.com>
2024-05-10 17:13:24 +0700

    update server js

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'src')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'index.html'));
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + '/src/index.html');
 });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
+  console.log('Server listening on port ${PORT}...');
 });
